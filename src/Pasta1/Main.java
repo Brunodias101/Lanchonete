0411f5b53bccf6d69aed8fe2cf749889eca0e715
Brunodias101 <bruno101dias@gmail.com>
2021-03-08 20:05:15 -0300

    atualização

--- src/Pasta1/Main.java
+++ src/Pasta1/Main.java
@@ -9,93 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
         //Definição de variavel
-<<<<<<< HEAD
-        int quantidadeP1 = 0, quantidadeP2 =0;
-        double total, totalP1 = 0, totalP2 =0 ;
-=======
         double total = 0;
->>>>>>> 31ac6f1 (segundo commit)
         int escolha = 8;
-        double pedido;
 
-<<<<<<< HEAD
-        //Criação do Scanner, nomeado a entrada de "scanner"
-        Scanner scanner = new Scanner(System.in);
-
-        Metodos utilizar = new Metodos();
-
-        //criacao de objeto-produto
-
-        Produto x_burguer = new Produto("X-burguer", "pão trad, burguer bovino 140g, maionese branca da casa", 13.90);
-        Produto x_salada = new Produto("X-salada", "pão trad, burguer bovino 140g, queijo pratro, alface, tomate, cebola, picles e maionese de salsa e alho",14.90);
-
-
-        for (int parametro = 0; parametro <= escolha; ) {
-            utilizar.primeiraEscolha();
-            parametro = scanner.nextInt();
-            if (parametro == 1) {
-                System.out.println("Descrição do X-burguer: "+x_burguer.getDescricao());
-                System.out.println("Preço: "+x_burguer.getPreco());
-
-                utilizar.segundaEscolha();
-                System.out.println("selecione a opção");
-
-                utilizar.setOpcao(scanner.nextInt());
-
-
-                switch (utilizar.getOpcao()) {
-                    case 1:
-                        System.out.println("Quantos " + x_burguer.getNome() + " gostaria de comprar:");
-                        quantidadeP1 += (quantidadeP1 = scanner.nextInt());
-                        totalP1 =  (13.90 * quantidadeP1);
-                        pedido= totalP1;
-                        System.out.println("Foi adicionado "+pedido+" a sua compra");
-
-                        break;
-
-                    case 2:
-                        break;
-
-                    default:
-                        System.out.println("Escolha invalida");
-                        break;
-
-
-                }
-
-            }
-            if (parametro == 2) {
-
-                System.out.println("Descrição do X-salada: "+x_salada.getDescricao());
-                System.out.println("Preço: "+x_salada.getPreco());
-
-                utilizar.segundaEscolha();
-                System.out.println("selecione a opção");
-
-                utilizar.setOpcao(scanner.nextInt());
-
-
-                switch (utilizar.getOpcao()) {
-                    case 1:
-                        System.out.println("Quantos " + x_salada.getNome() + " gostaria de comprar:");
-                        quantidadeP2 += (quantidadeP2 = scanner.nextInt());
-                        totalP2 =  (14.90 * quantidadeP2);
-                        pedido= totalP2;
-                        System.out.println("Foi adicionado "+pedido+" a sua compra");
-
-                        break;
-
-                    case 2:
-                        break;
-
-                    default:
-                        System.out.println("Escolha invalida");
-                        break;
-
-
-                }
-
-=======
         Produto[] produtos = criarProduto();
         Menu utilizar = new Menu();
         for (int parametro = 0; parametro <= escolha;) {
@@ -115,7 +31,6 @@ public class Main {
             }
             if (parametro==5) {
                 total += criarPedido(produtos[parametro - 1], utilizar);
->>>>>>> 31ac6f1 (segundo commit)
             }
             if (parametro==6) {
                 total+= criarPedido(produtos[parametro - 1], utilizar);
@@ -154,13 +69,8 @@ public class Main {
                 System.out.println("Escolha invalida");
                 break;
         }
-<<<<<<< HEAD
-        total= totalP1 + totalP2 ;
-        System.out.println("O valor total do pedido é de R$ "+total);
-=======
 
         return total;
->>>>>>> 31ac6f1 (segundo commit)
     }
 
     private static Produto[] criarProduto() {
@@ -176,5 +86,3 @@ public class Main {
         return produtos;
     }
 }
-
-
